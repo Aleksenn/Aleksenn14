5ebab8450cdf85f72c6412271fb49c3fab18acb1
Aleksenn <Aleksenn@users.noreply.github.com>
2014-10-09 21:14:46 +0400

    Create Den' nedeli.cpp

---
+++ Den' nedeli.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int to, tm, ye;
+	cout << "the day: \n";
+	cin >> to;
+	to = to % 7;
+	if (to == 7) {
+		to = to + 7;
+		cout << "today:\n" << endl;
+	}
+	tm = to + 1;
+	cout << "tomorrow:\n" << tm<< endl;
+	ye = to - 1;
+	if (ye <= 0) {
+		ye = ye + 7;
+	}
+	cout <<"yesterday:\n"<< ye << endl;
+	return 0;
+
+}
