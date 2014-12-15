940e530a1107f544d00f4e2f05d2c60fd2d746d7
Aleksenn <Aleksenn@users.noreply.github.com>
2014-12-15 20:38:34 +0300

    Update Delenie na 3.cpp

--- Delenie na 3.cpp
+++ Delenie na 3.cpp
@@ -10,6 +10,11 @@ int main(void)
 	int n;
 	cout << "¬ведите произвольное число " << endl;
 	cin >> n;
+	if (std::cin.fail())
+	{
+		std::cout << "Неккоректный ввод!" << std::endl;
+		return 0;
+	}
 	if (n % 3 == 0)
 	{
 		cout << "„исло делитс€ нацело на 3" << endl;
