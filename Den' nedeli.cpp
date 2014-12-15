692dbbc1ae45601182ef09dfb8be66e04e55b388
Aleksenn <Aleksenn@users.noreply.github.com>
2014-12-15 20:38:48 +0300

    Update Den' nedeli.cpp

--- Den' nedeli.cpp
+++ Den' nedeli.cpp
@@ -7,6 +7,11 @@ int main(void)
 	int to, tm, ye;
 	cout << "the day: \n";
 	cin >> to;
+	if (std::cin.fail())
+	{
+		std::cout << "Неккоректный ввод!" << std::endl;
+		return 0;
+	}
 	to = to % 7;
 	if (to == 7) {
 		to = to + 7;
