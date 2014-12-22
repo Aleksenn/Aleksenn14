6353671bbbf00d3ceb7c5937ffbd75f9219a10ac
Aleksenn <Aleksenn@users.noreply.github.com>
2014-12-22 21:54:43 +0400

    Update Den' nedeli.cpp

--- Den' nedeli.cpp
+++ Den' nedeli.cpp
@@ -11,7 +11,7 @@ int main(void)
         {       cout << "incorrectly input" << endl;
 		return 0;
 	}
-           cout « "tomorrow: " << ((to == 7) ? "1" : to + 1) << endl;
-           cout «"yesterday: " << ((to == 1) ? "7" : to - 1) << endl;
+           cout << "tomorrow: " << ((to == 7) ? "1" : to + 1) << endl;
+           cout << "yesterday: " << ((to == 1) ? "7" : to - 1) << endl;
            return 0;
 }        
