e3847d01eb17a6599275f635a9ed179f4723339c
Aleksenn <Aleksenn@users.noreply.github.com>
2014-12-22 21:50:21 +0400

    Update Den' nedeli.cpp

--- Den' nedeli.cpp
+++ Den' nedeli.cpp
@@ -4,25 +4,14 @@
 using namespace std;
 int main(void)
 {
-	int to, tm, ye;
+	int to;
 	cout << "the day: \n";
 	cin >> to;
 	if ((to>7)||(to<1)) 
         {       cout << "incorrectly input" << endl;
 		return 0;
 	}
-	to = to % 7;
-	if (to == 7) {
-		to = to + 7;
-		cout << "today:\n" << endl;
-	}
-	tm = to + 1;
-	cout << "tomorrow:\n" << tm<< endl;
-	ye = to - 1;
-	if (ye <= 0) {
-		ye = ye + 7;
-	}
-	cout <<"yesterday:\n"<< ye << endl;
-	return 0;
-
-}
+           cout « "tomorrow: " << ((to == 7) ? "1" : to + 1) << endl;
+           cout «"yesterday: " << ((to == 1) ? "7" : to - 1) << endl;
+           return 0;
+}        
